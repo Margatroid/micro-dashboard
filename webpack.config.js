db6f2da4a46ac0fb6bbb0b6d8a65ecaf9642ffd9
Alvis Duong <alvisduong@gmail.com>
2015-10-25 21:00:30 +0000

    Install babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
   output: {
     filename: 'app.js',
     path: __dirname + '/dist'
+  },
+
+  module: {
+    loaders: [
+      {
+	test: /\.js$/,
+	exclude: /node_modules/,
+	loaders: ['babel-loader']
+      }
+    ]
   }
 }
