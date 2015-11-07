3ffd7942514766ab09735f0a4e36e7dde439b7a7
Alvis Duong <alvisduong@gmail.com>
2015-11-07 23:29:48 +0000

    Style app bar

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import { AppBar, FontIcon, Tabs, Tab } from 'material-ui'
+import { AppBar, FontIcon, Styles, Tabs, Tab } from 'material-ui'
+const { Colors } = Styles
 
 export default React.createClass({
   render: function() {
-    const icon = <FontIcon className='material-icons' color='white'>cloud</FontIcon>
+    const iconStyle = {
+      marginTop: 11,
+      marginLeft: 20,
+      marginRight: 20
+    };
+    const icon = <FontIcon style={iconStyle}
+      className='material-icons'
+      color={Colors.grey50}>cloud</FontIcon>
 
     return <div>
       <AppBar iconElementLeft={icon} title='Micro dashboard'>
-	<Tabs>
-	  <Tab label='Explorer' />
-	  <Tab label='Query Tool' />
-	  <Tab label='API' />
+	<Tabs style={{width: 400}}>
+          <Tab label='EXPLORER' />
+          <Tab label='QUERY TOOL' />
+          <Tab label='API' />
 	</Tabs>
       </AppBar>
     </div>
