2a9752bfe98c93da949d910540899993ccd32fe1
Alvis Duong <alvisduong@gmail.com>
2015-11-07 23:07:04 +0000

    Add top navigation

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import { AppBar, FontIcon } from 'material-ui'
+import { AppBar, FontIcon, Tabs, Tab } from 'material-ui'
 
 export default React.createClass({
   render: function() {
-    const icon = <FontIcon className="material-icons" color='white'>cloud</FontIcon>
-    return <AppBar iconElementLeft={icon} title='Micro dashboard'/>
+    const icon = <FontIcon className='material-icons' color='white'>cloud</FontIcon>
+
+    return <div>
+      <AppBar iconElementLeft={icon} title='Micro dashboard'>
+	<Tabs>
+	  <Tab label='Explorer' />
+	  <Tab label='Query Tool' />
+	  <Tab label='API' />
+	</Tabs>
+      </AppBar>
+    </div>
   }
 })
