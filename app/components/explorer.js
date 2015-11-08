27fe14103884293eb23c42a0ef0c1906c35ef8ce
Alvis Duong <alvisduong@gmail.com>
2015-11-08 16:13:27 +0000

    Style Registry page

--- app/components/explorer.js
+++ app/components/explorer.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Paper, Styles } from 'material-ui'
+
+const Typography = Styles.Typography
 
 export default React.createClass({
   displayName: 'Explorer',
 
   render: function() {
-    return <div>Explorer</div>
+    const styles = {
+      wrapper: {
+        maxWidth: 960,
+        margin: '100 auto 0 auto'
+      },
+      header: {
+        fontSize: '24px',
+        paddingTop: '16px',
+        marginBottom: '12px',
+        letterSpacing: '0',
+        fontWeight: Typography.fontWeightNormal,
+        color: Typography.textDarkBlack,
+        fontFamily: 'Roboto, sans-serif'
+      },
+      paper: {
+        padding: 40
+      }
+    }
+
+    return <section style={styles.wrapper}>
+      <h1 style={styles.header}>Registry</h1>
+
+      <Paper style={styles.paper} zDepth={1} rounded={false}>
+      </Paper>
+    </section>
   }
 })
