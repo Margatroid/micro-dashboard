e4f2fd4c2f807e9b036a49dcec8234286c671169
Alvis Duong <alvisduong@gmail.com>
2015-12-05 17:21:23 +0000

    Write a basic proto to JSON converter
    Based loosely on the proto3 specs. Not robust enough for anything
    serious so perfect for the query editor where the user can correct any
    mistakes manually for now.

--- app/reducers/helpers.js
+++ app/reducers/helpers.js
@@ -1,9 +1,58 @@
+function formatEndpoint(input) {
+  let output = {}
+  if (!input) return output
+
+  input.forEach((field) => {
+    let fieldValue
+
+    switch (field.Type) {
+      // Strings
+      case 'enum':
+      case 'string':
+      case 'bytes':
+      case 'int64':
+      case 'fixed64':
+      case 'uint64':
+      case 'Timestamp':
+      case 'Duration':
+      case 'FieldMask':
+        fieldValue = ''
+        break
+      // Number
+      case 'int32':
+      case 'fixed32':
+      case 'uint32':
+      case 'float':
+      case 'float64':
+      case 'double':
+        fieldValue = 0
+        break
+      // Leave these types alone for now
+      case 'message':
+      case 'map':
+      case 'Any':
+      case 'Struct':
+      case 'ListValue':
+        fieldValue = field.Values
+        break
+      default:
+        fieldValue = formatEndpoint(field.Values)
+        break
+    }
+
+    output[field.Name] = fieldValue
+  })
+
+  return output
+}
+
 const helpers = {
+  // Turns the endpoint's proto3 into a JSON compatible request body.
   getRequestBody: function(service, method) {
     for (let index in service.Endpoints) {
       let endpoint = service.Endpoints[index]
       if (endpoint.Name == method) {
-        return JSON.stringify(endpoint.Request, null, 4)
+        return JSON.stringify(formatEndpoint(endpoint.Request.Values), null, 4)
       }
     }
   }
