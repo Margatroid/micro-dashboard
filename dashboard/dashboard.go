71e6e2a3544f4bbd5a7ab3992878e0ba67db0fc9
Alvis Duong <alvisduong@gmail.com>
2016-01-02 00:40:50 +0000

    Add HTTP server

--- dashboard/dashboard.go
+++ dashboard/dashboard.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"fmt"
 	log "github.com/golang/glog"
 	"github.com/micro/go-micro/cmd"
-	"github.com/micro/go-micro/server"
+	"net"
+	"net/http"
 )
 
 func main() {
 	cmd.Init()
 
-	server.Init(
-		server.Name("go.micro.web.dashboard"),
-	)
-
-	if err := server.Run(); err != nil {
-		log.Fatal(err)
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("Failed to create listener %s", err)
 	}
+
+	log.Infof("Listening on %s", listener.Addr().String())
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world!")
+	})
+
+	err = http.Serve(listener, nil)
 }
